96e6992e4c10eafab3d9ac1b3cf8364cdb3c2d34
Dinis Rodrigues <denis.dr4@gmail.com>
2021-10-07 23:14:46 +0100

    created the index page

---
+++ pages/index.tsx
@@ -0,0 +1,69 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+import ReactFullpage, { fullpageApi } from "@fullpage/react-fullpage";
+import { useState } from "react";
+import Navbar from "../components/Navbar/Navbar";
+
+import CoverSection from "../components/Index/CoverSection";
+import About from "../components/Index/About";
+import SolarSection from "../components/Index/SolarSection";
+import HydrogenSection from "../components/Index/HydrogenSection";
+import AutonomousSection from "../components/Index/AutonomousSection";
+import MlSection from "../components/ML/MlSection";
+
+const Home: NextPage = () => {
+  const navColors = [
+    "white",
+    "white",
+    "white",
+    "white",
+    "white",
+    "white",
+    "white",
+  ];
+  const [navTheme, setNavTheme] = useState("white");
+  const [startAboutZoom, setStartAboutZoom] = useState(false);
+  const [solarCount, setSolarCount] = useState(false);
+  const [playVideo, setPlayVideo] = useState(false);
+  return (
+    <div className="App">
+      <Head>
+        <title>Técnico Solar Boat</title>
+      </Head>
+      <Navbar theme={navTheme} />
+      <ReactFullpage
+        navigation
+        responsiveWidth={1250}
+        // pluginWrapper={pluginWrapper}
+        onLeave={(origin, destination, direction) => {
+          console.log("onLeave", { origin, destination, direction });
+          destination.index === 1
+            ? setStartAboutZoom(true)
+            : setStartAboutZoom(false);
+          destination.index === 2 || destination.index === 3
+            ? setSolarCount(true)
+            : setSolarCount(false);
+          destination.index === 4 ? setPlayVideo(true) : setPlayVideo(false);
+          setNavTheme(navColors[destination.index]);
+        }}
+        // scrollHorizontally = {true}
+        // sectionsColor={state.sectionsColor}
+        render={(comp) => (
+          <ReactFullpage.Wrapper>
+            <CoverSection />
+            <About startZoom={startAboutZoom} initialDepartment={"dc"} />
+            <SolarSection startCount={solarCount} />
+            <HydrogenSection startCount={solarCount} />
+            <AutonomousSection playVideo={playVideo} />
+            <MlSection />
+          </ReactFullpage.Wrapper>
+        )}
+      />
+    </div>
+  );
+};
+
+export default Home;
