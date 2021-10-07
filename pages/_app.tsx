1a3774704bc24a5a690beb239e1e186a4b17556d
Dinis Rodrigues <denis.dr4@gmail.com>
2021-10-07 23:14:33 +0100

    nextjs app initializer

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+// add bootstrap css
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.css";
+
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </Head>;
+  return <Component {...pageProps} />;
+}
+export default MyApp;
