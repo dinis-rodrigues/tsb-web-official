e26594434bfb5d066f9e8700e0751d2d322b6e73
Dinis Rodrigues <denis.dr4@gmail.com>
2021-10-10 02:52:01 +0100

    added team interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -1,3 +1,33 @@
+import { string } from "@tensorflow/tfjs-core";
+
+// Public Information
+export interface PublicTeam {
+  [key: string]: PublicUser;
+}
+
+export interface PublicUser {
+  info: PublicUserInfo;
+}
+
+export interface PublicUserInfo {
+  name: string;
+  position: string;
+  degree: string;
+  birth: string;
+  department: string;
+  joinedIn: string;
+  leftIn?: string;
+  linkedin?: string;
+  description?: string;
+  email?: string;
+  inTeam: boolean;
+}
+
+export interface DepartmentTab {
+  name: string;
+  active: boolean;
+}
+
 export interface ChartValue {
   x: number;
   [key: string]: number;
