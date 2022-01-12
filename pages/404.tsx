6218043d584753f0047cc75fe340b46b02d380ba
Dinis Rodrigues <denis.dr4@gmail.com>
2022-01-12 11:57:33 +0000

    feature: Added 404 and 500 custom page

---
+++ pages/404.tsx
@@ -0,0 +1,58 @@
+import ReactFullpage from "@fullpage/react-fullpage";
+import Head from "next/head";
+import Link from "next/link";
+import BottomWaves from "../components/Animations/BottomWaves";
+import GlowingStars from "../components/Animations/GlowingStars";
+import Universe from "../components/Animations/Universe";
+import FaviconIcons from "../components/Head/FaviconIcons";
+
+const Custom404 = () => {
+  return (
+    <div className="App">
+      <Head>
+        <title>Page not Found</title>
+        <FaviconIcons />
+      </Head>
+
+      <ReactFullpage
+        scrollOverflow
+        fitToSection={true}
+        navigation={false}
+        responsiveWidth={1250}
+        verticalCentered
+        render={({ fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div className="section bg-black" style={{ fontSize: "small" }}>
+                <div className="container stars">
+                  {/* This is only for large screens */}
+                  <div className="row h-100">
+                    <div className="col-12 flex-column d-flex justify-content-center align-items-center z-inf">
+                      <h1 className="very-large-font">404</h1>
+                      <h2>Page Not Found</h2>
+                      <h5>
+                        The Page You Are Attempting To Reach Is Currently Not
+                        Available. This May Be Because The Page Does Not Exist
+                        Or Has Been Moved.
+                      </h5>
+                      <Link passHref href={"/"}>
+                        <a>
+                          <button className="btnd btnd-info">Go Home</button>
+                        </a>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+                <GlowingStars />
+                <BottomWaves />
+                <Universe />
+              </div>
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
+    </div>
+  );
+};
+
+export default Custom404;
---
+++ pages/500.tsx
@@ -0,0 +1,57 @@
+import ReactFullpage from "@fullpage/react-fullpage";
+import Head from "next/head";
+import Link from "next/link";
+import BottomWaves from "../components/Animations/BottomWaves";
+import GlowingStars from "../components/Animations/GlowingStars";
+import Universe from "../components/Animations/Universe";
+import FaviconIcons from "../components/Head/FaviconIcons";
+
+const Custom500 = () => {
+  return (
+    <div className="App">
+      <Head>
+        <title>Internal Error</title>
+        <FaviconIcons />
+      </Head>
+
+      <ReactFullpage
+        scrollOverflow
+        fitToSection={true}
+        navigation={false}
+        responsiveWidth={1250}
+        verticalCentered
+        render={({ fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div className="section bg-black" style={{ fontSize: "small" }}>
+                <div className="container stars">
+                  {/* This is only for large screens */}
+                  <div className="row h-100">
+                    <div className="col-12 flex-column d-flex justify-content-center align-items-center z-inf">
+                      <h1 className="very-large-font">500</h1>
+                      <h2>{"An error occurred on our side 😖"}</h2>
+                      <h5>
+                        Something went wrong when we were fueling up the
+                        information. Please try again later.
+                      </h5>
+                      <Link passHref href={"/"}>
+                        <a>
+                          <button className="btnd btnd-info">Go Home</button>
+                        </a>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+                <GlowingStars />
+                <BottomWaves />
+                <Universe />
+              </div>
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
+    </div>
+  );
+};
+
+export default Custom500;
