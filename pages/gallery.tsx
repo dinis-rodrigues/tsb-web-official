b95cda112b1811bb0d268f4f1a55a32ce72b021a
Dinis Rodrigues <denis.dr4@gmail.com>
2022-01-12 11:58:01 +0000

    refactor: Gallery optimization

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,11 +1,22 @@
+/* eslint-disable react/display-name */
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable no-prototype-builtins */
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import GallerySidebar from "../components/Gallery/GallerySidebar";
 import GalleryInfo from "../components/Gallery/GalleryInfo";
 import { Controller, Scene } from "react-scrollmagic";
+
+import LightGallery from "lightgallery/react";
+
+// import plugins if you need
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+import lgAutoplay from "lightgallery/plugins/autoplay";
+import lgFullscreen from "lightgallery/plugins/fullscreen";
+import lgRotate from "lightgallery/plugins/rotate";
+import lgShare from "lightgallery/plugins/share";
 
 import {
   getGalleryList,
@@ -13,9 +24,9 @@ import {
 } from "../components/Gallery/galleryUtils";
 
 import Navbar from "../components/Navbar/Navbar";
-import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox";
 import { AllAlbumPhotos, GalleryItem } from "../interfaces";
 import GalleryHeader from "../components/Gallery/GalleryHeader";
+import FaviconIcons from "../components/Head/FaviconIcons";
 
 const Gallery: NextPage = () => {
   const [galleryInfo, setGalleryInfo] = useState<GalleryItem>();
@@ -25,6 +36,11 @@ const Gallery: NextPage = () => {
   const [navTheme, setNavTheme] = useState<"white" | "black">("white");
   useEffect(() => {
     getGalleryList(setGalleryList, setGalleryInfo, setActiveGallery);
+
+    window.scroll({
+      top: 1,
+      left: 0,
+    });
   }, []);
 
   useEffect(() => {
@@ -38,74 +54,81 @@ const Gallery: NextPage = () => {
       );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [activeGallery]);
+
   return (
     <div className="App">
       <Head>
-        <title>Técnico Solar Boat</title>
+        <title>Gallery</title>
+        <FaviconIcons />
       </Head>
 
-      <Navbar theme={navTheme} />
-      <GalleryHeader />
-
-      <div id="triggerEl">
-        <div className="container">
-          <div className="row nav-margin">
-            <div className="col-lg-2">
-              <GallerySidebar
-                galleryList={galleryList}
-                activeGallery={activeGallery}
-                setActiveGallery={setActiveGallery}
-              />
-            </div>
-            <div className="col" style={{ minHeight: "120vh" }}>
-              <SimpleReactLightbox>
-                <SRLWrapper>
-                  <div className="row">
-                    {galleryPhotos.hasOwnProperty(activeGallery) &&
-                    Object.entries(galleryPhotos[activeGallery]).length > 0
-                      ? Object.entries(galleryPhotos[activeGallery]).map(
-                          ([photoId, photoInfo]) => (
-                            <div
-                              className="col-sm-4 col-md-4 col-lg-3"
-                              key={photoId}
-                            >
-                              <div className="gallery-item">
-                                <div className="gallery-image">
-                                  <a href={photoInfo.imagePath}>
-                                    <img
-                                      src={photoInfo.rzImgPath}
-                                      alt={photoInfo.description}
-                                    />
-                                  </a>
-                                </div>
-                              </div>
+      <Controller>
+        <GalleryHeader />
+        <Scene
+          offset={-100}
+          triggerHook="onLeave"
+          triggerElement="#triggerEl"
+          // indicators={true}
+        >
+          {(progress: 0 | 1, event: string) => {
+            // This throws a forwardRef warning. Don't care
+            return <Navbar theme={progress} />;
+          }}
+        </Scene>
+        <div id="triggerEl">
+          <div className="container">
+            <div className="row nav-margin">
+              <div className="col-lg-2">
+                <GallerySidebar
+                  galleryList={galleryList}
+                  activeGallery={activeGallery}
+                  setActiveGallery={setActiveGallery}
+                />
+              </div>
+              <div className="col" style={{ minHeight: "120vh" }}>
+                {galleryPhotos.hasOwnProperty(activeGallery) &&
+                Object.entries(galleryPhotos[activeGallery]).length > 0 ? (
+                  <LightGallery
+                    mode="lg-fade"
+                    speed={500}
+                    plugins={[
+                      lgThumbnail,
+                      lgZoom,
+                      lgAutoplay,
+                      lgFullscreen,
+                      lgRotate,
+                      lgShare,
+                    ]}
+                    elementClassNames="row"
+                  >
+                    {Object.entries(galleryPhotos[activeGallery]).map(
+                      ([photoId, photoInfo]) => (
+                        <a
+                          href={photoInfo.imagePath}
+                          className="col-sm-4 col-md-4 col-lg-3"
+                          key={photoId}
+                        >
+                          <div className="gallery-item cursor-pointer">
+                            <div className="gallery-image">
+                              <img
+                                src={photoInfo.rzImgPath}
+                                alt={photoInfo.description}
+                              />
                             </div>
-                          )
-                        )
-                      : null}
-                  </div>
-                </SRLWrapper>
-              </SimpleReactLightbox>
+                          </div>
+                        </a>
+                      )
+                    )}
+                  </LightGallery>
+                ) : null}
+              </div>
+              <div className="col-lg-2">
+                <GalleryInfo galleryInfo={galleryInfo} />
+              </div>
             </div>
-            <div className="col-lg-2">
-              <GalleryInfo galleryInfo={galleryInfo} />
-            </div>
-            <Controller>
-              <Scene
-                offset={-100}
-                triggerHook="onLeave"
-                triggerElement="#triggerEl"
-                // indicators={true}
-              >
-                {(progress: number) => {
-                  progress === 1 ? setNavTheme("black") : setNavTheme("white");
-                  return <Fragment></Fragment>;
-                }}
-              </Scene>
-            </Controller>
           </div>
         </div>
-      </div>
+      </Controller>
     </div>
   );
 };
