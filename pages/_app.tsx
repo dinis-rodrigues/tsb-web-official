c0065ecc10990a33bacd87296c0d468da64ca362
Dinis Rodrigues <denis.dr4@gmail.com>
2022-01-12 12:04:44 +0000

    refactor: import new styles used throughout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,24 @@
 // add bootstrap css
 import "bootstrap/dist/css/bootstrap.css";
-import "../styles/globals.css";
+// If you want you can use SCSS instead of css
+import "lightgallery/scss/lightgallery.scss";
+import "lightgallery/scss/lg-zoom.scss";
+import "lightgallery/scss/lg-thumbnail.scss";
+import "lightgallery/scss/lg-autoplay.scss";
+import "lightgallery/scss/lg-fullscreen.scss";
+import "lightgallery/scss/lg-rotate.scss";
+import "lightgallery/scss/lg-share.scss";
+
+// My styles
+import "../styles/globals.scss";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import FaviconIcons from "../components/Head/FaviconIcons";
 
 function MyApp({ Component, pageProps }: AppProps) {
   <Head>
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-    />
+    <FaviconIcons />
   </Head>;
   return <Component {...pageProps} />;
 }
