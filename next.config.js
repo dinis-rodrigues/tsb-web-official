7ecc038ff6f3ee1fe3a69c629b098b98b430549d
Dinis Rodrigues <denis.dr4@gmail.com>
2022-01-12 12:03:10 +0000

    refactor: Production and development options

--- next.config.js
+++ next.config.js
@@ -1,7 +1,30 @@
 /** @type {import('next').NextConfig} */
+
+const DEVELOPMENT = 0;
+const PRODUCTION = 1;
+
+// Choose your environment
+const CURRENT_ENVIRONMENT = PRODUCTION;
+
+// Development and production export paths
+const developmentBasePathUrl = "";
+const productionBasePathUrl = "/~ist179089/projects/tsbOfficialDemo";
+let basePathUrl = "";
+
+if (CURRENT_ENVIRONMENT === DEVELOPMENT) {
+  basePathUrl = developmentBasePathUrl;
+} else {
+  basePathUrl = productionBasePathUrl;
+}
+
 module.exports = {
+  env: {
+    BASE_PATH: basePathUrl,
+  },
   reactStrictMode: true,
+  basePath: basePathUrl,
   images: {
-    domains: ["tsb.tecnico.ulisboa.pt"],
+    domains: ["tsb.tecnico.ulisboa.pt", "tecnicosolarboat.tecnico.ulisboa.pt"],
+    loader: "custom",
   },
 };
