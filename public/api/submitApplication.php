8e583f3b87cbcff418b0dea9df991e5cfc945304
Dinis Rodrigues <dinis.rodrigues@tecnico.ulisboa.pt>
2022-03-10 20:49:25 +0000

    feat: added composer installation

--- public/api/submitApplication.php
+++ public/api/submitApplication.php
@@ -1,6 +1,12 @@
 <?php
-// require_once('php\recaptchalib.php');
-require '../api/phpmailer/PHPMailerAutoload.php';
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 include "../protected/mysqlVariables.php";
 include "../protected/emailVariables.php";
 include "../protected/captcha_variables.php";
