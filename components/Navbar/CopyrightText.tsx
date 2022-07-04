ce4ff4a8c6de4601237723c10fa6e535fa27d60e
Dinis Rodrigues <dinis.rodrigues@tecnico.ulisboa.pt>
2022-07-04 02:01:45 +0100

    feat: adds link to website repository

--- components/Navbar/CopyrightText.tsx
+++ components/Navbar/CopyrightText.tsx
@@ -1,7 +1,13 @@
 const CopyrightText = () => {
   return (
     <div className={"copy f-medium d-none d-sm-block footer-text"}>
-      © Técnico Solar Boat {new Date().getFullYear()}
+      <a
+        href="https://github.com/dinis-rodrigues/tsb-web-official"
+        target={"_blank"}
+        rel="noreferrer"
+      >
+        © Técnico Solar Boat {new Date().getFullYear()}
+      </a>
     </div>
   );
 };
