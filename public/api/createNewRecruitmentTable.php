341e39879b83f1c10765ef566f1ed0b2b7baa9d4
Dinis Rodrigues <dinis.rodrigues@tecnico.ulisboa.pt>
2022-09-13 20:42:28 +0100

    updated deprecated file

--- public/api/createNewRecruitmentTable.php
+++ public/api/createNewRecruitmentTable.php
@@ -1,4 +1,7 @@
 <?php
+
+// This code no longer doES anything, DEPECRECATED TO REMOVE IN THE FUTURE
+
 include 'firebaseAuth.php';
 $res = ["success" => false, "msg" => ""];
 
@@ -24,16 +27,16 @@ if ($_POST) {
 	}
 
 	try {
-		$conn = new mysqli($sqlServername, $sqlUsername, $sqlPassword, $sqlDbname);
-		$sql = "CREATE TABLE $activeTable LIKE DO_NOT_DELETE_Recruta_Template";
+		// $conn = new mysqli($sqlServername, $sqlUsername, $sqlPassword, $sqlDbname);
+		// $sql = "CREATE TABLE $activeTable LIKE DO_NOT_DELETE_Recruta_Template";
 
 		// Check if it was sent to the database
 		// console_log($conn->query($sql));
-		if ($conn->query($sql) === TRUE) {
+		if (TRUE) {
 			$res["msg"] = "Table created successfully!";
 			$res["success"] = true;
 		} else {
-			$res["msg"] = "There was a problem creating the table. Please contact the admin.";
+			$res["msg"] = "Error";
 			$res["success"] = false;
 		}
 		echo json_encode($res);
